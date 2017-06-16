48e732d22bf153ccbe584f9826cb81e554f9388e
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2017-06-16 19:42:30 +0100

    Connect to MongoDB

--- Stream2Project.py
+++ Stream2Project.py
@@ -2,9 +2,18 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
+import os
  
 app = Flask(__name__)
- 
+
+#The os module will determine which environment variables to use from where they are supplied from
+#Local Connection
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'Stream2Project-LOCAL-DB')
+#Remote Connection
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://<dbuser>:<dbpassword>@ds129442.mlab.com:29442/heroku_2z1cv575')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'heroku_2z1cv575')
+
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 DBS_NAME = 'stream2project'
@@ -35,12 +44,13 @@ def stream2_projects():
  
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+    # The MONGO_URI connection is required when hosted using a remote mongo db.
+    with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
-        # and limit the the results to 55000
-        projects = collection.find(projection=FIELDS, limit=55000)
+        # and limit the the results to a lower limit of 20000
+        projects = collection.find(projection=FIELDS, limit=20000)
         # Convert projects to a list in a JSON object and return the JSON data
         return json.dumps(list(projects))
  
